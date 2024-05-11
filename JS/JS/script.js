0df6997a1977760f11c8c2e4ad9138bb5c4d28f8
Maitê Heloisa <164665315+Maymoon001@users.noreply.github.com>
2024-05-11 11:56:13 -0300

    update

---
+++ JS/JS/script.js
@@ -0,0 +1,28 @@
+/**/ //  comentarios 
+
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement
+const accordionHeaders =  document.querySelectorAll(".accordion-header")
+const menuLinks = document.querySelectorAll(".menu-link")
+
+function changeTheme() {
+    const currentTheme = rootHtml.getAttribute("data-theme")
+
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+// Função para alternar Tema "dark" / "light"
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+
+})
